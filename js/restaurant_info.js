11b001ed5f40a2b7b77edeff254c7dd02f06fc9e
John Christensen <public.requiem@gmail.com>
2018-06-02 18:55:55 +0300

    refactor: Minor refactors, updated port setting

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -140,7 +140,7 @@ createReviewHTML = (review) => {
  * Add restaurant name to the breadcrumb navigation menu
  */
 fillBreadcrumb = (restaurant=self.restaurant) => {
-  const breadcrumb = document.getElementById('breadcrumb');
+  const breadcrumb = document.getElementById('breadcrumbs');
   const li = document.createElement('li');
   li.innerHTML = restaurant.name;
   breadcrumb.appendChild(li);
